37ec77366401bd1efee553be093c6be3ce3b8898
Matt Wilkinson <117099989+matt-wilkinson-js@users.noreply.github.com>
2023-08-01 15:05:26 +0100

    js comment

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,3 +1,4 @@
+// this is a javascript to build out default datatables in markdown.
 document$.subscribe(function() {
     var tables = document.querySelectorAll("article table:not([class])")
     tables.forEach(function(table) {
